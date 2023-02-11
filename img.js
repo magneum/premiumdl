080877995815cdc573b5e9dfccd352ece99185a8
magneum <krakinzkon@gmail.com>
2023-02-11 09:10:14 +0530

    index

--- img.js
+++
@@ -1,5 +0,0 @@
-const getColors = require("get-image-colors");
-
-getColors("https://source.unsplash.com/random/640x480/").then((colors) => {
-  console.log(colors[0]._rgb._unclipped.join(","));
-});
