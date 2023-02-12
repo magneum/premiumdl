fe85bcc7e4f9af243438fc904a34de8c130e8668
magneum <krakinzkon@gmail.com>
2023-02-12 15:45:44 +0530

    premiumdl

--- pages/api/music.ts
+++ pages/api/music.ts
@@ -1,3 +1,4 @@
+const { GetAudio, GetVideo } = require("y2mate-api");
 import { youtubedl, youtubedlv2 } from "@bochilteam/scraper";
 import type { NextApiRequest, NextApiResponse } from "next";
 
@@ -7,13 +8,14 @@ export default async function search(
 ) {
   try {
     const query = req.query.q as string;
-    const yt = await youtubedl(query).catch(
-      async () => await youtubedlv2(query)
-    );
+    const _data = await GetAudio(query);
+    const data_ = await GetVideo(query);
     return res.send({
-      _audio: await yt.audio["128kbps"].download(),
-      _video: await yt.video["1080p"].download(),
+      _audio: _data.urlDown,
+      _video: data_.urlDown,
     });
+    // await yt.video["1080p"].download(),
+    // const yt = await youtubedl(query).catch(async () => await youtubedlv2(query) );
   } catch (error: any) {
     return res.status(500).json({
       status: "error",
